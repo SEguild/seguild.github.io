b8fecbb1734289398586628ec986dd90869bc030
BlacKYYYEaglE <bleagle@hotmail.de>
2023-11-12 17:38:06 +0100

    added abilities overlay template

--- guides/overlay.js
+++ guides/overlay.js
@@ -2,20 +2,23 @@ let boss = "";
 
 const overlayMap = {
     gnarlRoot: {
-        boss: "Boss: Gnarlroot <br/><br/>",
+        boss: "Boss: Gnarlroot <br/>",
         abilities: [
-            "Ability: Flaming Pestilence - Spawns lashers adds in AoE line.<br/><br/>",
-            "[Individual] Ability: Controlled Burn - Marks players with AoE circle. Drops fire pool. Pools stay until intermission.<br/><br/>",
-            "[Healer] Ability: Tortured Scream - Raidwide 10s DoT.<br/><br/>",
-            "[Tank] Ability: Dreadfire Barrage - Physical and shadow damage. Stacks Shadowflame vulnerability.<br/><br/>",
-            "[Intermission] Start: Kill all adds. Gnarlroot spawns roots. Boss sets players on fire - touch the roots to burn them.<br/><br/>",
-            "[Intermission] End: Intermission ends when all roots are burned. Boss takes 100% more damage for 20s.<br/><br/>",
+            "- Ability: Flaming Pestilence - Spawns lashers adds in AoE line.<br/>",
+            "- [Individual] Ability: Controlled Burn - Marks players with AoE circle. Drops fire pool. Pools stay until intermission.<br/>",
+            "- [Healer] Ability: Tortured Scream - Raidwide 10s DoT.<br/>",
+            "- [Tank] Ability: Dreadfire Barrage - Physical and shadow damage. Stacks Shadowflame vulnerability.<br/>",
+            "- [Intermission] Start: Kill all adds. Gnarlroot spawns roots. Boss sets players on fire - touch the roots to burn them.<br/>",
+            "- [Intermission] End: Intermission ends when all roots are burned. Boss takes 100% more damage for 20s.<br/>",
         ],
         hc: [
-            "[HC] Changes: Adds do not spawn automatically. Use Controlled Burn to activate them.<br/><br/>",
-            "[HC] Changes: Intermission - Players need to set themselves on fire to be able to burn the roots.<br/><br/>",
+            "- [HC Changes]: Adds do not spawn automatically. Use Controlled Burn to activate them.<br/>",
+            "- [HC Changes]: [Intermission] - Players need to set themselves on fire (soak) to be able to burn the roots.<br/>",
         ],
-        mythic: [],
+        mythic: [
+            "- [Mythic Changes] Spawns two treants on each add wave. Spread & interrupt.<br/>",
+            "- [Mythic Changes] [Intermission] - More roots, missing [HC Changes] soak causes raidwide damage.<br/>",
+        ],
     },
     igira: {
         boss: "Boss: Igira <br/><br/>",
@@ -60,10 +63,10 @@ const overlayMap = {
         mythic: [],
     },
     fyrakk: {
-        boss: "Boss: Fyrakk <br/><br/>",
-        abilities: [],
-        hc: [],
-        mythic: [],
+        boss: "Boss: Fyrakk <br/>",
+        abilities: ["- Abilities: Unknown.<br/>"],
+        hc: ["- [HC Changes] Unknown.<br/>"],
+        mythic: ["- [Mythic Changes] Unknown.<br/>"],
     },
 };
 
@@ -72,6 +75,7 @@ function openOverlay(param) {
     document.getElementById("abilityOverlay").style.display = "block";
     document.getElementById("bossAbilities").innerHTML =
         overlayMap[param].boss +
+        "<br/>" +
         overlayMap[param].abilities.reduce((acc, cur) => acc + cur, "") +
         "<br/>" +
         overlayMap[param].hc.reduce((acc, cur) => acc + cur, "") +
